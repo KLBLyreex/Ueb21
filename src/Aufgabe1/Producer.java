9198c851fc2ed8c9ba71a55361b512b4f8950999
Aaron <aaronjens@live.de>
2019-06-23 13:02:16 +0200

    Aufgabe 1 Kommentar Producer Klasse

--- src/Aufgabe1/Producer.java
+++ src/Aufgabe1/Producer.java
@@ -2,10 +2,20 @@ package Aufgabe1;
 
 import java.util.Random;
 
+/**
+ * @author Aaron Betzholz / Jan Ehrhardt
+ * @version 23.06.2019
+ *
+ * Producer Klasse, erzeugt eine zufaellige Zahl
+ */
 public class Producer {
 
     private Random rnd = new Random();
 
+    /**
+     * Gibt eine Zufaellige Zahl zurueck (Zwischen 0 und 1000)
+     * @return zufaellige Zahl
+     */
     public int zufallsZahl(){
         return rnd.nextInt(1001);
     }
